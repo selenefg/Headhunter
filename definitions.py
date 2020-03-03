98e6fc3f8af63fa25b9ffcd2414a1591e5d44372
selenefg <selene.delaflor@gmail.com>
2020-03-03 12:56:05 +0100

    rewrite descriptions

--- definitions.py
+++ definitions.py
@@ -11,21 +11,30 @@ HTTPHeaderEntries = {
 }
 
 SecHeadersDescriptions = {
-  "STS": "\n\tHTTP Strict Transport Security is an excellent feature to support on your site and strengthens\n"
-         "\tyour implementation of TLS by getting the User Agent to enforce the use of HTTPS.\n"
+  "STS": "\n\tHTTP Strict Transport Security (HSTS) is a web security policy mechanism which helps to protect websites\n"
+         "\tagainst protocol downgrade attacks and cookie hijacking. It allows web servers to declare that web browsers\n"
+         "\t(or other complying user agents) should only interact with it using secure HTTPS connections, and never via\n"
+         "\tthe insecure HTTP protocol."
          "\tRecommended value: \"strict-transport-security: max-age=31536000; includeSubDomains\".",
-  "XFrame": "\n\tX-Frame-Options tells the browser whether you want to allow your site to be framed or not.\n"
-            "\tBy preventing a browser from framing your site you can defend against attacks like clickjacking.\n"
-            "\tRecommended value: \"x-frame-options: SAMEORIGIN\".",
-  "XSS": "\n\tX-XSS-Protection sets the configuration for the cross-site scripting filter built into most browsers.\n"
+  "XFrame": "\n\tX-Frame-Options response header improve the protection of web applications against Clickjacking.\n"
+            "\tIt declares a policy communicated from a host to the client browser on whether the browser must not\n"
+            "\tdisplay the transmitted content in frames of other web pages.\n"
+            "\tRecommended value: \"x-frame-options: SAMEORIGIN; allow-from: DOMAIN; deny\".",
+  "XSS": "\n\tThis header enables the Cross-site scripting (XSS) filter in your browser.\n"
          "\tRecommended value: \"X-XSS-Protection: 1; mode=block\".",
-  "XContent": "\n\tX-Content-Type-Options stops a browser from trying to MIME-sniff the content type and forces it\n"
-              "\tto stick with the declared content-type.\n"
+  "XContent": "\n\tSetting this header will prevent the browser from interpreting files as something else than\n"
+              "\tdeclared by the content type in the HTTP headers.\n"
               "\tThe only valid value for this header is: \"X-Content-Type-Options: nosniff\".",
-  "Content": "\n\tContent Security Policy is an effective measure to protect your site from XSS attacks.\n"
-             "\tBy whitelisting sources of approved content, you can prevent the browser from loading malicious assets.",
+  "Content": "\n\tA Content Security Policy (CSP) requires careful tuning and precise definition of the policy.\n"
+             "\tIf enabled, CSP has significant impact on the way browsers render pages (e.g., inline JavaScript\n"
+             "\tdisabled by default and must be explicitly allowed in policy). CSP prevents a wide range of attacks,\n"
+             "\tincluding Cross-site scripting and other cross-site injections.",
   "XPerm": "TO-DO",#TODO
   "Referrer": "TO-DO",#TODO
+  #Public Key Pinning Extension for HTTP (HPKP)
+  #X-Permitted-Cross-Domain-Policies
+  #Expect-CT
+  #Feature-Policy
 }
 
 TransferEncondingHeader = {
